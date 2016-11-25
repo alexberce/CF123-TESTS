84b1828ce7f7fa6dee16534e4f5cdfe2bdbfa4c1
alexandru.berce@gmail.com <gik12q12q>
2016-11-26 00:53:43 +0200

    removed unused imports

--- src/test/java/TCommon/TCommonMain.java
+++ src/test/java/TCommon/TCommonMain.java
@@ -2,14 +2,9 @@ package TCommon;
 
 
 import Main.*;
-
-import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TCommonMain {
