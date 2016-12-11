18f0e7cc44fc491c3e0c67f054b0c122e7183f68
alexandru.berce@gmail.com <gik12q12q>
2016-12-12 00:55:43 +0200

    added getQueryParams method

--- src/main/java/Main/WindowHandler.java
+++ src/main/java/Main/WindowHandler.java
@@ -2,6 +2,13 @@ package Main;
 
 import static org.junit.Assert.*;
 import org.openqa.selenium.WebDriver;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class WindowHandler {
@@ -29,6 +36,35 @@ public class WindowHandler {
 
     public void goToPage(String url){
         this.driver.get(url);
+    }
+
+    public static Map<String, List<String>> getQueryParams(String url) {
+        try {
+            Map<String, List<String>> params = new HashMap<String, List<String>>();
+            String[] urlParts = url.split("\\?");
+            if (urlParts.length > 1) {
+                String query = urlParts[1];
+                for (String param : query.split("&")) {
+                    String[] pair = param.split("=");
+                    String key = URLDecoder.decode(pair[0], "UTF-8");
+                    String value = "";
+                    if (pair.length > 1) {
+                        value = URLDecoder.decode(pair[1], "UTF-8");
+                    }
+
+                    List<String> values = params.get(key);
+                    if (values == null) {
+                        values = new ArrayList<String>();
+                        params.put(key, values);
+                    }
+                    values.add(value);
+                }
+            }
+
+            return params;
+        } catch (UnsupportedEncodingException ex) {
+            throw new AssertionError(ex);
+        }
     }
 
     public void switchToIframe(String iframeId){
